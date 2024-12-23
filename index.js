8f98f74d8c92cc066e83da74b03f4d85d145031a
Jadon <1tenletters0@gmail.com>
2024-12-23 15:45:35 -0800

    What the hell

--- index.js
+++ index.js
@@ -6,27 +6,29 @@ function contact(event) {
   event.preventDefault();
   const loading = document.querySelector(".modal__overlay--loading");
   const sucess = document.querySelector(".modal__overlay--success");
-
+//   loading.classList += " modal__overlay--visible";
+  
   emailjs
     .sendform(
       "service_dmf4e7a",
       "template_acfbkmf",
       event.target,
       "qVGgK80-uMkkItvbU"
-    )
-    .then(() => {
-      throw new Error("error");
+    ).then(() => {
+        throw new Error('error');
       loading.classList.remove("modal__overlay--visible");
       sucess.classList += " modal__overlay--visible";
-    }) .catch(() => {
+    }).catch(() => {
       loading.classList.remove("modal__overlay--visible");
       alert(
         "The email service is temporarily unavailable. Please contact me directly at 1tenletters0@gmail.com"
       );
     });
 
-  loading.classList += " modal__overlay--visible";
-  setTimeout(() => {
-    console.log("It worked 1");
-  }, 1000);
 }
+
+loading.classList += " modal__overlay--visible";
+  setTimeout(() => {
+   
+    console.log('it worked 1')
+  }, 1000)
