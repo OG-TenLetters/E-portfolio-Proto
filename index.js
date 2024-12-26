1cb4546552a9e65dd7aad087600a01e549885908
Jadon <1tenletters0@gmail.com>
2024-12-26 11:20:52 -0800

    Background images not possible... and the work-arounds aren't even working... Now I have to restyle everything.. great

--- index.js
+++ index.js
@@ -1,34 +1,83 @@
+let isModalOpen = false;
+let contrastToggle = false
+
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+        document.body.classList += " dark-theme"
+    }
+    else {
+        document.body.classList.remove("dark-theme")
+    }
+}
+
+
 // Template Key: template_acfbkmf
 // Service Key: service_dmf4e7a
 // API key : qVGgK80-uMkkItvbU
 
+// function contact(event) {
+//   event.preventDefault();
+//   const loading = document.querySelector(".modal__overlay--loading");
+//   const success = document.querySelector(".modal__overlay--success");
+//   loading.classList += " modal__overlay--visible";
+//   emailjs
+
+//     .sendform("service_dmf4e7a", "template_acfbkmf", event.target)
+//     .then(() => {
+//       loading.classList.remove("modal__overlay--visible");
+//       success.classList += " modal__overlay--visible";
+//     })
+//     .catch(() => {
+//       loading.classList.remove("modal__overlay--visible");
+//       alert(
+//         "The email service is temporarily unavailable. Please contact me directly at 1tenletters0@gmail.com"
+//       );
+//     });
+// }
+
 function contact(event) {
   event.preventDefault();
+
   const loading = document.querySelector(".modal__overlay--loading");
-  const sucess = document.querySelector(".modal__overlay--success");
-//   loading.classList += " modal__overlay--visible";
-  
+
+  const success = document.querySelector(".modal__overlay--success");
+
+  loading.classList += " modal__overlay--visible";
+
   emailjs
-    .sendform(
+
+    .sendForm(
       "service_dmf4e7a",
       "template_acfbkmf",
       event.target,
       "qVGgK80-uMkkItvbU"
-    ).then(() => {
-        throw new Error('error');
+    )
+
+    .then(() => {
       loading.classList.remove("modal__overlay--visible");
-      sucess.classList += " modal__overlay--visible";
-    }).catch(() => {
+
+      success.classList += " modal__overlay--visible";
+    })
+
+    .catch(() => {
       loading.classList.remove("modal__overlay--visible");
+
       alert(
         "The email service is temporarily unavailable. Please contact me directly at 1tenletters0@gmail.com"
       );
     });
-
 }
 
-loading.classList += " modal__overlay--visible";
-  setTimeout(() => {
-   
-    console.log('it worked 1')
-  }, 1000)
+function toggleModal() {
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open");
+  }
+  isModalOpen = true;
+
+  //toggle modal
+  document.body.classList += "modal--open";
+}
+
