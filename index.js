400b2d006c2867c082541c9eac8a858da13232b2
Jadon <1tenletters0@gmail.com>
2024-12-27 11:59:01 -0800

    I need to fix the shapes and modal

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ function moveBackground(event) {
 
   for (let i = 0; i < shapes.length; ++i) {
     const isOdd = i % 2 !== 0
-    const oddInteger = isOdd ? -1 : 1
-    shapes[i].style.transform = `translate(${x * oddInteger}px, ${y * oddInteger}px)`
+    const boolInt = isOdd ? -1 : 1
+    shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
   }
 }
 
@@ -25,29 +25,6 @@ function toggleContrast() {
   }
 }
 
-// Template Key: template_acfbkmf
-// Service Key: service_dmf4e7a
-// API key : qVGgK80-uMkkItvbU
-
-// function contact(event) {
-//   event.preventDefault();
-//   const loading = document.querySelector(".modal__overlay--loading");
-//   const success = document.querySelector(".modal__overlay--success");
-//   loading.classList += " modal__overlay--visible";
-//   emailjs
-
-//     .sendform("service_dmf4e7a", "template_acfbkmf", event.target)
-//     .then(() => {
-//       loading.classList.remove("modal__overlay--visible");
-//       success.classList += " modal__overlay--visible";
-//     })
-//     .catch(() => {
-//       loading.classList.remove("modal__overlay--visible");
-//       alert(
-//         "The email service is temporarily unavailable. Please contact me directly at 1tenletters0@gmail.com"
-//       );
-//     });
-// }
 
 function contact(event) {
   event.preventDefault();
@@ -85,10 +62,10 @@ function contact(event) {
 function toggleModal() {
   if (isModalOpen) {
     isModalOpen = false;
-    return document.body.classList.remove("modal--open");
+    return document.body.classList.remove("modal--open")
   }
   isModalOpen = true;
 
   //toggle modal
-  document.body.classList += "modal--open";
+  document.body.classList += " modal--open";
 }
